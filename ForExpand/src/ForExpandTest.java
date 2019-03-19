0866f2a05d7e9f7add15512aee6f1dcaaa950bdf
hyemijung84 <hyemijung84@gmail.com>
2019-03-19 11:08:55 +0900

    작성자: HM
    작성내용: Foreach문에 내 이름출력

--- ForExpand/src/ForExpandTest.java
+++ ForExpand/src/ForExpandTest.java
@@ -12,15 +12,17 @@ public class ForExpandTest {
 			numList.add(i+1);
 		}
 				
-		for (Integer num : numList) {
-			System.out.println(num);
-		}
+//		for (Integer num : numList) {
+//			System.out.println(num);
+//		}
 		
-		String[] strArr = new String[3];
+		String[] strArr = new String[5];
 		
-		strArr[0] = "글자";
-		strArr[1] = "222";
-		strArr[2] = "글자 222";
+		strArr[0] = "정혜미1";
+		strArr[1] = "정혜미2";
+		strArr[2] = "정혜미3";
+		strArr[3] = "정혜미4";
+		strArr[4] = "정혜미5";
 		
 		for (String str : strArr) {
 			System.out.println(str);
