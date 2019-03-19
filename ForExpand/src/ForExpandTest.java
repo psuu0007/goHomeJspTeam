081a72fea9b330ddf9d99db886c96380b1e347e2
fit.ssung <fit.ssung@gmail.net>
2019-03-19 11:10:02 +0900

    작성자: 정민성
    내용
    내가 먼저 올린다!

--- ForExpand/src/ForExpandTest.java
+++ ForExpand/src/ForExpandTest.java
@@ -8,19 +8,19 @@ public class ForExpandTest {
 
 		List<Integer> numList = new ArrayList<Integer>();
 		
-		for (int i = 0; i < 5; i++) {
-			numList.add(i+1);
-		}
-				
-		for (Integer num : numList) {
-			System.out.println(num);
-		}
+//		for (int i = 0; i < 5; i++) {
+//			numList.add(i+1);
+//		}
+//				
+//		for (Integer num : numList) {
+//			System.out.println(num);
+//		}
 		
 		String[] strArr = new String[3];
 		
-		strArr[0] = "글자";
-		strArr[1] = "222";
-		strArr[2] = "글자 222";
+		strArr[0] = "정민성1";
+		strArr[1] = "정민성2";
+		strArr[2] = "정민성3";
 		
 		for (String str : strArr) {
 			System.out.println(str);
