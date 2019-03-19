2e2fe80ef3371af2b637b48e4588aaead5780e53
psuu0007 <psuu0007@naver.com>
2019-03-19 11:03:46 +0900

    작성자: 강사
    확장된 for문
    foreach문

---
+++ ForExpand/src/ForExpandTest.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForExpandTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> numList = new ArrayList<Integer>();
+		
+		for (int i = 0; i < 5; i++) {
+			numList.add(i+1);
+		}
+				
+		for (Integer num : numList) {
+			System.out.println(num);
+		}
+		
+		String[] strArr = new String[3];
+		
+		strArr[0] = "글자";
+		strArr[1] = "222";
+		strArr[2] = "글자 222";
+		
+		for (String str : strArr) {
+			System.out.println(str);
+		}
+		
+		
+	}
+
+}
